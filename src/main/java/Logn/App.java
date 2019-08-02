5ec922516e54db66333658ac9a4443f77eb44c3c
swag31415 <kidswagatam18@gmail.com>
2019-08-01 20:12:26 -0400

    added a simple texture to the model

--- src/main/java/Logn/App.java
+++ src/main/java/Logn/App.java
@@ -55,10 +55,12 @@ public class App extends Application {
 
     @FXML private void showModel(ActionEvent event) {
         ObjModelImporter meshImporter = new ObjModelImporter();
+        
         meshImporter.read(getClass().getClassLoader().getResource("Bawl.obj"));
-        // stlImporter.read(getClass().getClassLoader().getResource("CylinderHead-binary.stl"));
-
-        mesh.setMesh(meshImporter.getImport()[0].getMesh());
+        MeshView imp = meshImporter.getImport()[0];
+        
+        mesh.setMesh(imp.getMesh());
+        mesh.setMaterial(imp.getMaterial());
     }
 
     public static void main(String[] args) {
