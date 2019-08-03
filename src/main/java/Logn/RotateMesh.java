ca0d5d79d7dc3cdfdf22786e12d286e555fc69d1
swag31415 <kidswagatam18@gmail.com>
2019-08-03 13:10:18 -0400

    Made RotateMesh accept any Node not just MeshViews

--- src/main/java/Logn/RotateMesh.java
+++ src/main/java/Logn/RotateMesh.java
@@ -1,16 +1,17 @@
 package Logn;
 
 import Logn.Keyer.Press;
+import javafx.scene.Node;
 import javafx.scene.shape.MeshView;
 import javafx.scene.transform.Rotate;
 
 public class RotateMesh implements Runnable {
 
-    private MeshView mesh;
+    private Node node;
     private Keyer keyer;
 
-    public RotateMesh(MeshView mesh, Keyer keyer) {
-        this.mesh = mesh;
+    public RotateMesh(Node node, Keyer keyer) {
+        this.node = node;
         this.keyer = keyer;
     }
 
@@ -21,22 +22,22 @@ public class RotateMesh implements Runnable {
 
         if (keyer.getKeyStates().get(Press.up)) {
             rX.setAngle(2);
-            mesh.getTransforms().add(rX);
+            node.getTransforms().add(rX);
         }
 
         if (keyer.getKeyStates().get(Press.down)) {
             rX.setAngle(-2);
-            mesh.getTransforms().add(rX);
+            node.getTransforms().add(rX);
         }
 
         if (keyer.getKeyStates().get(Press.left)) {
             rY.setAngle(-2);
-            mesh.getTransforms().add(rY);
+            node.getTransforms().add(rY);
         }
 
         if (keyer.getKeyStates().get(Press.right)) {
             rY.setAngle(2);
-            mesh.getTransforms().add(rY);
+            node.getTransforms().add(rY);
         }
     }
 
