825c7b1bbec797dbeaeab69f89eac3e8dd220922
swag31415 <kidswagatam18@gmail.com>
2019-08-10 12:21:55 -0400

    Deleted all java code and FXML then rebuilt the ball in JME

--- src/main/java/Logn/App.java
+++ src/main/java/Logn/App.java
@@ -1,95 +1,23 @@
 package Logn;
 
-import javafx.application.Application;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Point3D;
-import javafx.scene.AmbientLight;
-import javafx.scene.PerspectiveCamera;
-import javafx.scene.Scene;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.MeshView;
-import javafx.stage.Stage;
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.scene.Spatial;
+import com.jme3.texture.Texture;
 
-public class App extends Application {
-
-    @FXML
-    private StackPane stackPane;
-
-    @FXML
-    private AmbientLight light;
-
-    private FXMLLoader loader;
-    private Scene scene;
-    private PerspectiveCamera camera;
-
-    Keyer keyer;
-    Processor processor;
-
-    boolean initalized;
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        loader = new FXMLLoader();
-        loader.setLocation(getClass().getClassLoader().getResource("UI.fxml"));
-        stackPane = loader.<StackPane>load();
-
-        scene = new Scene(stackPane, stackPane.getWidth(), stackPane.getHeight(), true);
-        camera = new PerspectiveCamera(true);
-        scene.setCamera(camera);
-        camera.translateXProperty().set(stackPane.getPrefWidth() / 2);
-        camera.translateYProperty().set(stackPane.getPrefHeight() / 2);
-        camera.setFarClip(30000);
-        camera.translateZProperty().set(-2000);
-
-        stackPane.requestFocus();
-
-        primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
-        primaryStage.show();
-    }
-
-    private void play() {
-        light.colorProperty().set(Color.WHITE);
-
-        MeshView sphere = MeshLoader.loadMesh("TheBawl/Bawl3.obj");
-        MeshView teapot = MeshLoader.loadMesh("TheTeapot/Teapot.obj");
-
-        stackPane.getChildren().addAll(sphere, teapot);
-
-        sphere.setScaleX(300);
-        sphere.setScaleY(300);
-        sphere.setScaleZ(300);
-
-        teapot.translateYProperty().set(- 300);
-
-        keyer = new Keyer();
-        processor = new Processor(40);
-
-        processor.addRunnable(new WASDRotateMesh(sphere, Point3D.ZERO, keyer));
-        processor.addRunnable(new WASDRotateMesh(teapot, new Point3D(0, 300, 0), keyer));
-        processor.addRunnable(new MousePan(keyer, stackPane.getScene().getWindow(), (PerspectiveCamera) stackPane.getScene().getCamera()));
-
-        processor.start();
-    }
-
-    @FXML
-    private void keyPressed(KeyEvent event) {
-        if (!initalized) {
-            play();
-            initalized = true;
-        }
-        keyer.reportKeyPressed(event.getCode());
-    }
-
-    @FXML
-    private void keyReleased(KeyEvent event) {
-        keyer.reportKeyReleased(event.getCode());
-    }
+public class App extends SimpleApplication {
 
     public static void main(String[] args) {
-        launch(args);
+        new App().start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        Spatial ball = assetManager.loadModel("TheBawl/Bawl3.obj");
+        Material testMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        Texture ballTexture = assetManager.loadTexture("TheBawl/UV.png");
+        testMat.setTexture("ColorMap", ballTexture);
+        ball.setMaterial(testMat);
+        rootNode.attachChild(ball);
     }
 }
